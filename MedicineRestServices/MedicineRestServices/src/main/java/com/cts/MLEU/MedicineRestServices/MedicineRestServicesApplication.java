9db1cd2657cc36771d6577924bf00edebc01bc69
modhtanmay <modhtanmay@gmail.com>
2020-10-23 16:04:49 +0530

    initial commit

---
+++ MedicineRestServices/MedicineRestServices/src/main/java/com/cts/MLEU/MedicineRestServices/MedicineRestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cts.MLEU.MedicineRestServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineRestServicesApplication.class, args);
+	}
+
+}
---
+++ MedicineRestServices/MedicineRestServices/src/test/java/com/cts/MLEU/MedicineRestServices/MedicineRestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.MLEU.MedicineRestServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineRestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
